b590bf61873b5a82b55e4e1e7843f690640402e6
jay <jacob.e.vickers@gmail.com>
2017-02-14 15:55:05 -0700

    stub contact and about modals. stub players table page - 2

---
+++ WebApplication3/Controllers/PlayersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+   
+    public class PlayersController : Controller
+    {
+        //private ApplicationSignInManager _signInManager;
+        //private ApplicationUserManager _userManager;
+
+        public PlayersController()
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        if (_userManager != null)
+        //        {
+        //            _userManager.Dispose();
+        //            _userManager = null;
+        //        }
+
+        //        if (_signInManager != null)
+        //        {
+        //            _signInManager.Dispose();
+        //            _signInManager = null;
+        //        }
+        //    }
+
+        //    base.Dispose(disposing);
+        //}
+
+        #region Helpers
+        // Used for XSRF protection when adding external logins
+        //private const string XsrfKey = "XsrfId";
+
+        //private IAuthenticationManager AuthenticationManager
+        //{
+        //    get
+        //    {
+        //        return HttpContext.GetOwinContext().Authentication;
+        //    }
+        //}
+
+        //private void AddErrors(IdentityResult result)
+        //{
+        //    foreach (var error in result.Errors)
+        //    {
+        //        ModelState.AddModelError("", error);
+        //    }
+        //}
+
+        //private ActionResult RedirectToLocal(string returnUrl)
+        //{
+        //    if (Url.IsLocalUrl(returnUrl))
+        //    {
+        //        return Redirect(returnUrl);
+        //    }
+        //    return RedirectToAction("Index", "Home");
+        //}
+
+        //internal class ChallengeResult : HttpUnauthorizedResult
+        //{
+        //    public ChallengeResult(string provider, string redirectUri)
+        //        : this(provider, redirectUri, null)
+        //    {
+        //    }
+
+        //    public ChallengeResult(string provider, string redirectUri, string userId)
+        //    {
+        //        LoginProvider = provider;
+        //        RedirectUri = redirectUri;
+        //        UserId = userId;
+        //    }
+
+        //    public string LoginProvider { get; set; }
+        //    public string RedirectUri { get; set; }
+        //    public string UserId { get; set; }
+
+        //    public override void ExecuteResult(ControllerContext context)
+        //    {
+        //        var properties = new AuthenticationProperties { RedirectUri = RedirectUri };
+        //        if (UserId != null)
+        //        {
+        //            properties.Dictionary[XsrfKey] = UserId;
+        //        }
+        //        context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
+        //    }
+        //}
+        #endregion
+    }
+}
